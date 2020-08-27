cb957592ba9ad666ccdcef7c5340ce470322f6e6
XNPIPI <xinongpipi@sina.com>
2020-08-27 18:07:37 +0800

    add WeightQuickUnionUF.java file

--- WeightQuickUnionUF.java
+++ WeightQuickUnionUF.java
@@ -4,17 +4,21 @@
  *  Last modified:     1/1/2019
  **************************************************************************** */
 
-public class QuickUnionUF {
+public class WeightQuickUnionUF {
     private int[] parent;
+    private int[] size;
     private int count;
 
-    public QuickUnionUF(int n) {
-        parent = new int[n];
+    public WeightQuickUnionUF(int n) {
         count = n;
+        parent = new int[n];
+        size = new int[n];
         for (int i = 0; i < n; i++) {
             parent[i] = i;
+            size[i] = 1;
         }
     }
+
 
     public int count() {
         return count;
@@ -27,29 +31,39 @@ public class QuickUnionUF {
         return p;
     }
 
-    private void validate(int p) {
-        int n = parent.length;
-        if (p < 0 || p >= n) {
-            throw new IllegalArgumentException("index " + p + " is not between 0 and" + (n - 1));
-        }
-    }
-
     @Deprecated
     public boolean connected(int p, int q) {
         return find(p) == find(q);
+    }
+
+    private void validate(int p) {
+
+        int n = parent.length;
+        if (p < 0 || p >= n) {
+            throw new IllegalArgumentException("index " + p + " is not betweent 0 and  " + (n - 1));
+        }
     }
 
     public void union(int p, int q) {
         int rootP = find(p);
         int rootQ = find(q);
         if (rootP == rootQ) return;
-        parent[rootP] = rootQ;
+        if (size[rootP] < size[rootQ]) {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        }
+        else {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+
+        }
         count--;
     }
 
+
     public static void main(String[] args) {
         int n = StdIn.readInt();
-        QuickUnionUF uf = new QuickUnionUF(n);
+        QuickFindUF uf = new QuickFindUF(n);
         while (!StdIn.isEmpty()) {
             int p = StdIn.readInt();
             int q = StdIn.readInt();
@@ -57,8 +71,8 @@ public class QuickUnionUF {
             uf.union(p, q);
             StdOut.println(p + " " + q);
 
-
         }
         StdOut.println(uf.count() + " components");
+
     }
 }
