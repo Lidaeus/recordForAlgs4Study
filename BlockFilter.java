b69377e8c46ebefd5cc944f06a797082a9d1f8f9
XNPIPI <xinongpipi@sina.com>
2020-09-01 17:04:23 +0800

    add new file

---
+++ BlockFilter.java
@@ -0,0 +1,26 @@
+/* *****************************************************************************
+ *  Name:              Alan Turing
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+public class BlockFilter {
+    private BlockFilter() {
+    }
+
+    public static void main(String[] args) {
+        SET<String> set = new SET<String>();
+        In in = new In(args[0]);
+        while (!in.isEmpty()) {
+            String word = in.readString();
+            set.add(word);
+        }
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            if (!set.contains(word))
+                StdOut.println(word);
+        }
+    }
+
+
+}
