8bddd4cd45c11d2279b07abd1071e17a105153f3
XNPIPI <xinongpipi@sina.com>
2020-10-02 14:57:54 +0800

    add new file.

---
+++ TransitiveClosure.java
@@ -0,0 +1,45 @@
+/* *****************************************************************************
+ *  Name:              Alan Turing
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+public class TransitiveClosure {
+    private DirectedDFS[] tc;
+
+    public TransitiveClosure(Digraph G) {
+        tc = new DirectedDFS[G.V()];
+        for (int v = 0; v < G.V(); v++) {
+            tc[v] = new DirectedDFS(G, v);
+        }
+    }
+
+    public boolean reachable(int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
+        return tc[v].marked(w);
+    }
+
+    private void validateVertex(int v) {
+        int V = tc.length;
+        if (v < 0 || v >= V) {
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V - 1));
+        }
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+        TransitiveClosure tc = new TransitiveClosure(G);
+
+        StdOut.print("    ");
+        for (int v = 0; v < G.V(); v++) {
+            StdOut.printf("%3d: ", v);
+            for (int w = 0; w < G.V(); w++) {
+                if (tc.reachable(v, w)) StdOut.printf("    T");
+                else StdOut.printf("    ");
+            }
+            StdOut.println();
+        }
+    }
+}
